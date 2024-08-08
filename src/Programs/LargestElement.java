3dc474b444d956f3b9e12c276a6eeb77f5aa232c
Sriganth G <srivkp27@gmail.com>
2024-08-08 20:59:06 +0530

    update

--- src/Programs/LargestElement.java
+++ src/Programs/LargestElement.java
@@ -5,12 +5,20 @@ public class LargestElement {
 	public static void main(String[] args) {
 		int [] arr = {2,3,4,10,9,8};
 		int max=arr[0];
+		int min=arr[0];
 		for(int i =1 ; i<arr.length; i++) {
 			if(arr[i]>max) {
 				max=arr[i];
 			}
 			}
+		for(int i=1;i<arr.length;i++) {
+			if(arr[i]<min) {
+				min=arr[i];
+			}
+			
+		}
 		System.out.println(max);
+		System.out.println(min);
 
 	}
 
